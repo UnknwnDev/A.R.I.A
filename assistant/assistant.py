aed1ff667987cdb238d2c3ea780cd1c4c5bc0af7
UnknwnDev <0unknwn.dev@gmail.com>
2025-04-25 10:26:27 -0700

    feat(chatbot): add conversation and data storage for chatbot

--- assistant/assistant.py
+++ assistant/assistant.py
@@ -1,14 +1,20 @@
 from assistant.speech import Speech
 from assistant.prompt_manager import PromptManager
 from assistant.ollama_llm import OllamaLLM
+from assistant.memory import Memory
 
 class Assistant:
     def __init__(self, use_voice=False):
         self.llm = OllamaLLM()
         self.prompter = PromptManager()
         self.speaker = Speech()
+        self.memory = Memory()
         self.history = []
         self.voice_enabled = use_voice
+        
+        # load history
+        data = self.memory.load_data()
+        self.history = data if data else []
 
     def chat_loop(self):
         print("Voice Assistant is live! Say 'exit' or 'quit' to stop.\n")
@@ -27,6 +33,9 @@ class Assistant:
 
             self.history.append({"role": "user", "content": user_input})
             self.history.append({"role": "assistant", "content": response})
+            
+            
+            self.memory.store_data(self.history[-2:])
 
             print(f"Assistant: {response}\n")
             self.speaker.speak(response)
---
+++ assistant/memory.py
@@ -0,0 +1,65 @@
+import json
+import datetime
+
+class Memory:
+    def __init__(self) -> None:
+        self.file_path = "data/memory.json"
+        self.current_date = datetime.date.today().strftime("%m/%d/%Y")
+    
+    def store_data(self, data=None, _type:str = "conversation") -> int:
+        if data is None:
+            return -1
+
+        
+        
+        with open(self.file_path, "r+") as file:
+            # Format
+
+            # Load existing data 
+            try:
+                file_data = json.load(file)
+            except Exception:
+                # File empty 
+                file_data = {}
+            
+            if self.current_date in file_data.keys():
+                if _type in file_data[self.current_date].keys():
+                    # print(f"Found {type} in {self.current_date}.")
+                    if type(file_data[self.current_date][_type]) is list:
+                        file_data[self.current_date][_type].extend(data[:])
+                    else:
+                        file_data[self.current_date][_type].update(data[:])
+                    
+                    file.seek(0)
+                else:
+                    file_data[self.current_date] = {_type: data}
+            else:
+                file_data = {self.current_date: {_type: data}}
+            
+            json.dump(file_data, file,  indent=2)
+                
+            
+            # Save the data to a file
+           
+        
+        # Data stored correctly
+        return 0
+
+    def load_data(self, _type:str = "conversation"):
+        data = None
+        # Open file
+        with open(self.file_path, "r") as file:
+            # Load existing data 
+            try:
+                file_data = json.load(file)
+            except Exception:
+                # File is empty
+                return None
+            
+            # Loads the days current data
+            if self.current_date in file_data.keys():
+                if _type in file_data[self.current_date].keys():
+                    data = file_data[self.current_date][_type]
+            
+        # Data found
+        return data
