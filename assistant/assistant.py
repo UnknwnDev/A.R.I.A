9c337eec68868aeb44c14925f62d62576e860469
UnknwnDev <0unknwn.dev@gmail.com>
2025-04-28 10:24:01 -0700

    feat: add intent of user manager

--- assistant/assistant.py
+++ assistant/assistant.py
@@ -2,12 +2,14 @@ from assistant.speech import Speech
 from assistant.prompt_manager import PromptManager
 from assistant.ollama_llm import OllamaLLM
 from assistant.memory import Memory
+from assistant.intent_manager import IntentManager
 
 
 class Assistant:
     def __init__(self, use_voice=False):
         self.llm = OllamaLLM()
         self.prompter = PromptManager()
+        self.intent_manager = IntentManager()
         self.speaker = Speech()
         self.memory = Memory()
         self.history = []
@@ -17,7 +19,7 @@ class Assistant:
         data = self.memory.load_data()
         self.history = data if data else []
 
-    def chat_loop(self):
+    def chat_loop(self) -> None:
         """Runs loop for user to speak with agent"""
         print("Voice Assistant is live! Say 'exit' or 'quit' to stop.\n")
 
@@ -25,18 +27,26 @@ class Assistant:
             user_input = input("You: ")
 
             if user_input.lower() in ("exit", "quit"):
+                self.chat("goodbye aria")
                 break
 
             if not user_input.strip():
                 continue
 
-            prompt = self.prompter.build_prompt(user_input)
-            response = self.llm.get_response(prompt, self.history)
+            intent = self.intent_manager.get_intent(user_input)
+            if "command" in intent:
+                self.speaker.speak(f"This is a {intent}")
+            else:
+                self.chat(user_input)
 
-            self.history.append({"role": "user", "content": user_input})
-            self.history.append({"role": "assistant", "content": response})
+    def chat(self, user_input: str) -> None:
+        prompt = self.prompter.build_prompt(user_input)
+        response = self.llm.get_response(prompt, self.history)
 
-            self.memory.store_data(self.history[-2:])
+        self.history.append({"role": "user", "content": user_input})
+        self.history.append({"role": "assistant", "content": response})
 
-            print(f"Assistant: {response}\n")
-            self.speaker.speak(response)
+        self.memory.store_data(self.history[-2:])
+
+        print(f"Assistant: {response}\n")
+        self.speaker.speak(response)
---
+++ assistant/intent_manager.py
@@ -0,0 +1,16 @@
+from assistant.ollama_llm import OllamaLLM
+
+
+class IntentManager:
+    def __init__(self) -> None:
+        self.system_prompt = """
+            You are Aria, an intelligent assistant. 
+            You are to classify the users input as either "command", "chat", "question".
+            Respond with only one word: 'command', 'chat', 'question'.
+            Note questions will not have '?' in them.
+            """
+        self.ollama = OllamaLLM()
+
+    def get_intent(self, user_input: str) -> str:
+        prompt = self.system_prompt + "\nUser: " + user_input + "\nAria:"
+        return self.ollama.get_response(prompt)
