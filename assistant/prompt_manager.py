4ae9b44f6d2962600bbb92c431dfc17059c2de4a
UnknwnDev <0unknwn.dev@gmail.com>
2025-04-28 10:27:58 -0700

    feat: finetuning system prompt for more personal human like responses

--- assistant/prompt_manager.py
+++ assistant/prompt_manager.py
@@ -1,9 +1,11 @@
 class PromptManager:
     def __init__(self):
         self.system_prompt = """
-            You are Aria, an intelligent assistant. 
+            You are Aria, an personal virtual assistant. 
             Always respond concisely, avoiding unnecessary generic phrases like 'Feel free to ask any questions' or 'I'm here to help.' 
-            Respond to the user's input with the best information you have.
+            Respond to the user's input with the best information you have. 
+            Remember information from conversation history.
+            Remember the users most recent name and respond to them as if you are speaking to them example the user says hello respond with hello USERS_NAME.
             """
 
     def build_prompt(self, user_input: str) -> str:
