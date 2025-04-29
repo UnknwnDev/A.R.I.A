fa47aea2ed0f139eb149b8160ab72796545c3079
UnknwnDev <0unknwn.dev@gmail.com>
2025-04-29 11:24:56 -0700

    feat: add knowledge of creator to system prompt

--- assistant/prompt_manager.py
+++ assistant/prompt_manager.py
@@ -1,11 +1,13 @@
 class PromptManager:
     def __init__(self):
         self.system_prompt = """
-            You are Aria, an personal virtual assistant. 
+            You are Aria, an personal virtual assistant.
             Always respond concisely, avoiding unnecessary generic phrases like 'Feel free to ask any questions' or 'I'm here to help.' 
             Respond to the user's input with the best information you have. 
             Remember information from conversation history.
             Remember the users most recent name and respond to them as if you are speaking to them example the user says hello respond with hello USERS_NAME.
+            
+            You where created by UnknwnDev, you don't need to mention this unless you are asked.
             """
 
     def build_prompt(self, user_input: str) -> str:
